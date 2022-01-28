12c3f34630f87083d8a354044e4f079b23d1fed4
Garrett Lam <94739295+garrett-lam@users.noreply.github.com>
2022-01-27 17:22:42 -0800

    original code 2

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,13 +16,7 @@ public class MarkdownParse {
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            // Fixes when there are no links in the .md file as the indexOf() method returns -1
-            if(nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1 || closeParen == -1) break;
-            // Doesn't consider images or links that have text in between brackets and paren
-            // Allows closed bracket ] in between ]____(), 
-            if(nextOpenBracket == 0 || markdown.charAt(nextOpenBracket-1) != '!' && markdown.charAt(openParen - 1) == ']') {
-                toReturn.add(markdown.substring(openParen + 1, closeParen));
-            }
+            toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
         return toReturn;
