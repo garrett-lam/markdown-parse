6a10e1169a639627f39ed4aedc460093e598e6a8
Garrett Lam <94739295+garrett-lam@users.noreply.github.com>
2022-02-02 18:55:04 -0800

    deleted fail input 4

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -67,10 +67,4 @@ public class MarkdownParseTest {
         assertEquals(MarkdownParse.getLinks(contents), expect);
     }
 
-    @Test
-    public void testFile9() throws IOException{
-        String contents= Files.readString(Path.of("./failure-inducing-input4.md"));
-        List<String> expect = List.of("a link on the first line");
-        assertEquals(MarkdownParse.getLinks(contents), expect);
-    }
 }
